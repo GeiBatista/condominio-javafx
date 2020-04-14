f1737387ae3af288487654775382d6b2f5486a50
GeiBatista <geibatista@hotmail.com>
2020-04-14 20:08:12 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -11,9 +12,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.VBox;
 import model.services.CondominioService;
 
@@ -29,43 +30,28 @@ public class MainViewController implements Initializable {
 	private MenuItem menuItemSobre;
 
 	@FXML
-	public void onMenuItemCondominioAction() {
-		carregarTela2("/gui/CondominioList.fxml");
-	}
-
-	@FXML
 	public void onMenuItemUnidadeAction() {
 		System.out.println("onMenuItemUnidadeAction");
 	}
 
 	@FXML
+	public void onMenuItemCondominioAction() {
+		carregarTela("/gui/CondominioList.fxml", (CondominioListController controller) -> {
+			controller.setCondominioService(new CondominioService());
+			controller.updateTableView();
+		});
+	}
+
+	@FXML
 	public void onMenuItemSobreAction() {
-		carregarTela("/gui/Sobre.fxml");
+		carregarTela("/gui/Sobre.fxml", x -> {});
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 	}
 
-	public synchronized void carregarTela(String nomeAbsoluto) {
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
-			VBox newVbox = loader.load();
-
-			Scene mainScene = Main.getMainScene();
-			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
-
-			Node mainMenu = mainVBox.getChildren().get(0);
-			mainVBox.getChildren().clear();
-			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVbox.getChildren());
-		
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception", "Erro ao carregar Tela", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	public synchronized void carregarTela2(String nomeAbsoluto) {
+	public synchronized <T> void carregarTela(String nomeAbsoluto, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto));
 			VBox newVbox = loader.load();
@@ -78,12 +64,12 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVbox.getChildren());
 			
-			CondominioListController controller = loader.getController();
-			controller.setCondominioService(new CondominioService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Erro ao carregar Tela", e.getMessage(), AlertType.ERROR);
 		}
 	}
+		
 }
